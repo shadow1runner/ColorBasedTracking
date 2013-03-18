d6c277b4a5b840b6885152e60cf009b44c390b50
Helmut Wolf <helmut.wolf@outlook.com>
2013-03-18 20:34:38 +0100

    very first commit: nearly finished version

---
+++ colorBasedTracking/src/at/ac/uibk/cs/auis/ColorBasedTracker.java
@@ -0,0 +1,118 @@
+package at.ac.uibk.cs.auis;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class ColorBasedTracker {
+
+	private static final double MINIMAL_VALUE_OF_CONTOUR_AREA = 0.1;
+	
+	private Mat blackWhiteMask;
+	private Mat dilatedMask;
+
+	private Scalar colorForTrackingHSV;
+	private Scalar colorRadius = new Scalar(25, 25, 25, 0);
+	private Scalar lowerBound;
+	private Scalar upperBound;
+
+	private List<MatOfPoint> contour;
+	private Rect boundingRect;
+	private static double MINIMAL_VALUE_OF_CONTOUR_ARE = 0.1;
+	
+	private void initializeBounds() {
+		lowerBound = new Scalar(0);
+		upperBound = new Scalar(0);
+		lowerBound.val[0] = colorForTrackingHSV.val[0] - colorRadius.val[0];
+		upperBound.val[0] = colorForTrackingHSV.val[0] + colorRadius.val[0];
+		lowerBound.val[1] = colorForTrackingHSV.val[1] - colorRadius.val[1];
+		upperBound.val[1] = colorForTrackingHSV.val[1] + colorRadius.val[1];
+		lowerBound.val[2] = colorForTrackingHSV.val[2] - colorRadius.val[2];
+		upperBound.val[2] = colorForTrackingHSV.val[2] + colorRadius.val[2];
+		lowerBound.val[3] = 0;
+		upperBound.val[3] = 255;
+	}
+
+	public Point calcCenterOfMass(Mat hsv) {
+		blackWhiteMask = new Mat();
+		Core.inRange(hsv, lowerBound, upperBound, blackWhiteMask);
+
+		dilatedMask = new Mat();
+		Imgproc.dilate(blackWhiteMask, dilatedMask, new Mat());
+
+		contour = new ArrayList<MatOfPoint>();
+		Mat mHierarchy = new Mat();
+		Mat tempDilatedMask = dilatedMask.clone();
+		Imgproc.findContours(tempDilatedMask, contour, mHierarchy,
+				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+		boundingRect = Imgproc.boundingRect(findLargenstContour(contour));
+
+		int centerOfMassX = boundingRect.x + boundingRect.width / 2;
+		int centerOfMassY = boundingRect.y + boundingRect.height / 2;
+
+		return new Point(centerOfMassX, centerOfMassY);
+	}
+
+	private MatOfPoint findLargenstContour(List<MatOfPoint> contours) {
+		// Find max contour area
+		MatOfPoint maxAreaMatrix = new MatOfPoint();
+
+		double maxArea = 0;
+
+		Iterator<MatOfPoint> each = contour.iterator();
+		while (each.hasNext()) {
+			MatOfPoint wrapper = each.next();
+			double area = Imgproc.contourArea(wrapper);
+			if (area > maxArea) {
+				maxArea = area;
+				maxAreaMatrix = wrapper;
+			}
+		}
+
+		if (maxArea < MINIMAL_VALUE_OF_CONTOUR_AREA)
+			throw new IllegalArgumentException();
+
+		return maxAreaMatrix;
+	}
+
+	public Scalar getColorForTrackingHSV() {
+		return colorForTrackingHSV;
+	}
+
+	public void setColorForTrackingHSV(Scalar colorForTrackingHSV) {
+		this.colorForTrackingHSV = colorForTrackingHSV;
+		initializeBounds();
+	}
+
+	public Scalar getColorRadius() {
+		return colorRadius;
+	}
+
+	public void setColorRadius(Scalar colorRadius) {
+		this.colorRadius = colorRadius;
+	}
+
+	public Mat getBlackWhiteMask() {
+		return blackWhiteMask;
+	}
+
+	public Mat getDilatedMask() {
+		return dilatedMask;
+	}
+
+	public Rect getBoundingRect() {
+		return boundingRect;
+	}
+	
+	
+
+}
---
+++ colorBasedTracking/src/at/ac/uibk/cs/auis/ColorBasedTrackingActivity.java
@@ -0,0 +1,262 @@
+package at.ac.uibk.cs.auis;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+
+public class ColorBasedTrackingActivity extends Activity implements
+		CvCameraViewListener2 {
+
+	private MenuItem[] mItems;
+
+	private static final String TAG = "OCVSample::Activity";
+
+	/** size of the frame in the middle of the screen */
+	private static final int PIXELS_OF_PREVIEW = 32;
+
+	private static final int SIZE_OF_CENTER_OF_MASS = 6;
+
+	/** thickness of rectangle in the middle of the screen */
+	private static final int THICKNESS_OF_RECTANGLE = 5;
+
+	private static final Scalar COLOR_OF_RECT = new Scalar(0xbf, 0xfe, 0x00,
+			0x00);
+
+	private CameraBridgeViewBase mOpenCvCameraView;
+
+	private boolean isTracking = false;
+	private boolean trackColor = true;
+
+	private ColorBasedTracker colorBasedTracker = new ColorBasedTracker();
+	private TrackerHelper trackerHelper = new TrackerHelper();
+	private ActivityMode selectedMenu = ActivityMode.TrackColorMode;
+
+	private enum ActivityMode {
+		TrackColorMode, // indicates that the user is currently choosing a color
+						// (which later on should be used for tracking the ROI)
+		BlackWhiteMode, // the inRange-function is displayed
+		DilateMode, // the dilated black-White-Frame is displayed
+		ContoursMode, // the contours are displayed
+		CenterOfMassModeBlackWhiteMode, // the center of mass is displayed in a
+										// black-white-frame
+		CenterOfMassRgbMode, // the center of mass is displayed in a rgb-frame
+		BoundingRectangleMode // displays the bounding rectangle in rgb-frame
+	}
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+				mOpenCvCameraView.enableView();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "called onCreate");
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		setContentView(R.layout.color_based_tracking_surface_view);
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_based_tracking_surface_view);
+		mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+		mOpenCvCameraView.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				isTracking = true;
+				mOpenCvCameraView.setClickable(false);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		mItems = new MenuItem[ActivityMode.values().length];
+		for (int i = 0; i < mItems.length; i++) {
+			mItems[i] = menu.add(ActivityMode.values()[i].toString());
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+		selectedMenu = ActivityMode.valueOf((String) item.getTitle());
+		return true;
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this,
+				mLoaderCallback);
+	}
+
+	public void onDestroy() {
+		super.onDestroy();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
+
+	public void onCameraViewStarted(int width, int height) {
+	}
+
+	public void onCameraViewStopped() {
+	}
+
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		Mat rgba = inputFrame.rgba();
+
+		if (isTracking == false)
+			return createRectangle(rgba, PIXELS_OF_PREVIEW, PIXELS_OF_PREVIEW,
+					THICKNESS_OF_RECTANGLE);
+
+		else {
+			Mat hsv = new Mat();
+			Imgproc.cvtColor(rgba, hsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+			if (trackColor == true) {
+				trackColor = false;
+				Point center = new Point(hsv.width() / 2, hsv.height() / 2);
+				colorBasedTracker.setColorForTrackingHSV(trackerHelper
+						.calcColorForTracking(hsv, center));
+			}
+			
+			Point centerOfMass = null;
+			try {
+				centerOfMass = colorBasedTracker.calcCenterOfMass(hsv);
+			} catch (IllegalArgumentException e) {
+			}
+			switch (selectedMenu) {
+			case BlackWhiteMode:
+				rgba = colorBasedTracker.getBlackWhiteMask();
+			case BoundingRectangleMode:
+				rgba = createRectangle(rgba,
+						colorBasedTracker.getBoundingRect());
+				break;
+
+			case CenterOfMassModeBlackWhiteMode:
+				Mat rgbaBwMask = new Mat();
+				Imgproc.cvtColor(colorBasedTracker.getDilatedMask(),
+						rgbaBwMask, Imgproc.COLOR_GRAY2BGRA);
+				rgba = drawCenterOfMass(rgbaBwMask, centerOfMass);
+				break;
+
+			case CenterOfMassRgbMode:
+				rgba = drawCenterOfMass(rgba, centerOfMass);
+				break;
+
+			case ContoursMode:
+				break;
+
+			case DilateMode:
+				colorBasedTracker.getDilatedMask();
+				break;
+
+			case TrackColorMode:
+				rgba = drawCenterOfMass(rgba, centerOfMass);
+				break;
+
+			}
+			return rgba;
+		}
+	}
+
+	private Mat createRectangle(Mat picture, int heigth, int width,
+			int borderSize) {
+		int OffsetXofRect = picture.width() / 2 - width / 2 - borderSize;
+		int OffsetYofRect = picture.height() / 2 - heigth / 2 - borderSize;
+
+		Point topLeft = new Point(OffsetXofRect - THICKNESS_OF_RECTANGLE,
+				OffsetYofRect - THICKNESS_OF_RECTANGLE);
+		Point bottomRight = new Point(OffsetXofRect + width
+				+ THICKNESS_OF_RECTANGLE, OffsetYofRect + heigth
+				+ THICKNESS_OF_RECTANGLE);
+		Core.rectangle(picture, topLeft, bottomRight, COLOR_OF_RECT,
+				THICKNESS_OF_RECTANGLE);
+		return picture;
+	}
+
+	private Mat createRectangle(Mat picture, Rect rec) {
+		Core.rectangle(picture, rec.tl(), rec.br(), COLOR_OF_RECT);
+		return picture;
+	}
+
+	private Mat drawCenterOfMass(Mat picture, Point centerOfMass) {
+		int centerMaxX = (int) (centerOfMass.x + SIZE_OF_CENTER_OF_MASS / 2);
+		int centerMinX = (int) (centerOfMass.x - SIZE_OF_CENTER_OF_MASS / 2);
+		int centerMaxY = (int) (centerOfMass.y + SIZE_OF_CENTER_OF_MASS / 2);
+		int centerMinY = (int) (centerOfMass.y - SIZE_OF_CENTER_OF_MASS / 2);
+
+		if (centerMaxX > picture.width() - 1)
+			centerMaxX = picture.width() - 1;
+
+		if (centerMaxY > picture.height() - 1)
+			centerMaxY = picture.height() - 1;
+
+		if (centerMinX < 0)
+			centerMinX = 0;
+
+		if (centerMinY < 0)
+			centerMinY = 0;
+
+		// for printing the bounding rectangle
+		// Core.rectangle(maskRGB, new Point(boundingRect.x,
+		// boundingRect.y),
+		// new Point(boundingRect.x + boundingRect.width, boundingRect.y +
+		// boundingRect.height),
+		// COLOR_OF_RECT, THICKNESS_OF_RECTANGLE);
+
+		Mat subMatColorMass = picture.submat(centerMinY, centerMaxY,
+				centerMinX, centerMaxX);
+		subMatColorMass.setTo(COLOR_OF_RECT);
+
+		return picture;
+	}
+
+	@Override
+	public void onBackPressed() {
+		startActivity(new Intent(this, ColorBasedTrackingActivity.class));
+		finish();
+	}
+
+}
